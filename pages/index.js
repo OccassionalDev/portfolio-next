ec00997acfbf9742e34df866213928a4dfd62cb2
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-09 00:26:31 -0500

    added icons to footer

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
@@ -26,7 +28,10 @@ export default function Home() {
 			<main className={styles.main}>
 				<div id="home"></div>
 
-				<div id="about"></div>
+				<div id="about">
+					<FontAwesomeIcon icon={faGithub} fixedWidth />
+					<FontAwesomeIcon icon={faLinkedinIn} fixedWidth />
+				</div>
 
 				<div id="projects"></div>
 
@@ -97,14 +102,8 @@ export default function Home() {
 			</main>
 
 			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
-					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-				</a>
+				<FontAwesomeIcon icon={faGithub} fixedWidth />
+				<FontAwesomeIcon icon={faLinkedinIn} fixedWidth />
 			</footer>
 		</div>
 	);
