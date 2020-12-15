a1103ced6707f2bfbbe67280add6126fcb84d288
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-14 23:32:16 -0500

    created a ProjectCard component that uses the ProjectModal component

---
+++ components/ProjectCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ProjectModal from "./ProjectModal";
+
+export class ProjectCard extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default ProjectCard;
