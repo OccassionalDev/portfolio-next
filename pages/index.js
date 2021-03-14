3475e34bc19582ebd5a552a621cd35953bc722a8
OccassionalDev <ericaschimmel01@gmail.com>
2021-03-14 14:39:48 -0400

    installed bootstrap, updated next

--- pages/index.js
+++ pages/index.js
@@ -31,18 +31,18 @@ export default function Home() {
 
 				<div id="about">
 					<h2>About Me</h2>
-					<SocialIcons />
+					<p>Hello there! I'm Erica,</p>
 				</div>
 
 				<div id="projects">
 					<h2>Featured Projects</h2>
+
+					<h3>Events App</h3>
+					<h3>Investments Planner</h3>
+					<h3>Storyline</h3>
 				</div>
 
-				<div id="contact">
-					<h2>Let's start working together.</h2>
-
-					<EmailForm />
-				</div>
+				<div id="contact"></div>
 
 				<div id="blog"></div>
 			</main>
